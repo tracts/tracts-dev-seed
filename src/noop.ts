73b7e1c481c3b020282339b2d059441ea45659d1
Erik Kemperman <erikkemperman@gmail.com>
2018-02-13 11:28:04 +0100

    Add typedoc comments to src/noop.ts

--- src/noop.ts
+++ src/noop.ts
@@ -1,8 +1,21 @@
 export class Noop {
 
+  /**
+   * First part of greeting message.
+   * @type {string}
+   */
   public hello: string = "Hello";
+
+  /**
+   * Second part of greeting message.
+   * @type {string}
+   */
   public world: string = "World";
 
+  /**
+   * Returns the greeting message.
+   * @returns {string}
+   */
   public hi() {
     return `${this.hello} ${this.world}`;
   }
